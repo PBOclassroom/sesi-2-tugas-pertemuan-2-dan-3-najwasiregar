342a5d6aef66f9d9192068cc50b9d74e89634806
najwasiregar <118334792+najwasiregar@users.noreply.github.com>
2023-09-18 16:15:26 +0700

    Add files via upload

---
+++ Tugas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus_
+ */
+public class Tugas {
+    public static void main(String[] args) {
+        //Membuat Variabel nama
+        String Nama = "Najwa Alawiyah Siregar";
+        //Membuat variabel Jurusan
+        String Jurusan = "Informatika";
+        //Membuat variabel NIM
+        String NIM = "22346040";
+        //Membuat variabel Hobi
+        String Hobi = "Dengerin musik";
+        
+        
+        //Kode untuk menampilkan Isi Dari Variabel Nama 
+        System.out.println(Nama);
+        //Kode untuk menampilkan Isi Dari Variabel Jurusan
+        System.out.println(Jurusan);
+        //Menampilkan isi dari variabel NIM
+        System.out.println(NIM);
+        //Menampilkan isi dari variabel Hobi
+        System.out.println (Hobi);
+        
+    }
+}
